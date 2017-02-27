670ec1df33d3cab3a8d91c8be9d8c846be2711ca
Anthony Cato <anthonycato@Anthonys-MacBook-Pro.local>
2017-02-26 20:04:43 -0500

    Remove unnecessary files

--- Course 3 Project/run_analysis.R
+++ Course 3 Project/run_analysis.R
@@ -28,7 +28,7 @@ get_aggregate <- function(data) {
 get_subject_data <- function() {
     train_subject_data <- fread(file.path(PATH, "train", "subject_train.txt"))
     test_subject_data <- fread(file.path(PATH, "test", "subject_test.txt"))
-    
+
     subject_combined <- rbind(train_subject_data, test_subject_data)
     names(subject_combined) <- c("Subject")
     return (subject_combined)
@@ -37,23 +37,23 @@ get_subject_data <- function() {
 get_measurements <- function() {
     train_X_data <- fread(file.path(PATH, "train", "X_train.txt"), select = wanted_feat_indices)
     test_X_data <- fread(file.path(PATH, "test", "X_test.txt"), select = wanted_feat_indices)
-    
+
     X_combined <- rbind(train_X_data, test_X_data)
-    names(X_combined) <- feature_wanted$feature_name 
+    names(X_combined) <- feature_wanted$feature_name
     return (X_combined)
 }
 
 get_activity_labels <- function() {
     activities <- c('WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS',
                     'SITTING', 'STANDING', 'LAYING')
-    
+
     train_y_data <- fread(file.path(PATH, "train", "y_train.txt"))
     test_y_data <- fread(file.path(PATH, "test", "y_test.txt"))
-    
+
     y_combined <- rbind(train_y_data, test_y_data)
     y_combined <- as.factor(unlist(y_combined))
     levels(y_combined) <- activities
-    
+
     return (y_combined)
 }
 
@@ -61,11 +61,11 @@ get_tidy_data <- function() {
     subjects <- get_subject_data()
     activity <- get_activity_labels()
     measurements <- get_measurements()
-    
+
     data <- cbind(subjects, activity, measurements)
-    
+
     data <- rename(data, Activity = activity)
-    
+
     agg_data <- get_aggregate(data)
     return (agg_data)
 }
@@ -75,5 +75,5 @@ agg_data <- get_tidy_data()
 options(warn = 0)
 response <- readline(prompt="Would you like to write this to file? (y/n) ")
 
-if (response == "y") 
-    write.table(agg_data, file = "output.txt")
+if (response == "y")
+    write.table(agg_data, file = "output.txt", row.names = FALSE)
