476843ca518d98e34c093d1f55d9d1bb962a435b
Anthony Cato <catoa0608@gmail.com>
2017-03-03 16:12:20 -0500

    Add plot 4 using ggplot

--- Course 4 Project/plot4.R
+++ Course 4 Project/plot4.R
@@ -1,5 +1,17 @@
 scc <- readRDS("summarySCC_PM25.rds")
 codes <- readRDS("Source_Classification_Code.rds")
 
-coalSCC <- codes[grepl("Combustion", codes$SCC.Level.One) | grepl("Coal", codes$SCC.Level.Four),]
+codes.coal <- codes[grepl("Combustion", codes$SCC.Level.One) | 
+                        grepl("Coal", codes$SCC.Level.Four), 1]
 
+scc <- subset(scc, SCC %in% codes.coal)
+
+agg <- aggregate(Emissions ~ year + type, scc, sum)
+
+png(filename = "plot4.png", height = 600, width = 800)
+
+p <- qplot(year, Emissions, data = agg) 
+p <- p + aes(color = type) 
+p <- p + geom_smooth()
+
+dev.off()
