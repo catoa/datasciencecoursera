3cd25e6606d62290f306bbb2156166a3a2fc45db
Anthony Cato <anthonycato@Anthonys-MacBook-Pro.local>
2017-03-03 21:02:28 -0500

    Add script and png for plot 6

--- Course 4 Project/plot5.R
+++ Course 4 Project/plot5.R
@@ -1,3 +1,6 @@
+require(data.table, quietly = T)
+require(ggplot2, quietly = T)
+
 scc <- readRDS("summarySCC_PM25.rds")
 codes <- readRDS("Source_Classification_Code.rds")
 
@@ -7,7 +10,7 @@ scc <- subset(scc, SCC %in% codes.vehicle & fips == "24510")
 
 agg <- aggregate(Emissions ~ year, scc, sum)
 
-png("plot5.png", width=840, height=480)
+png(filename = "plot5.png", width=840, height=480)
 p <- ggplot(data = agg, aes(factor(year), Emissions))
 p <- p + geom_bar(stat="identity") +
     xlab("year") +
---
+++ Course 4 Project/plot6.R
@@ -0,0 +1,27 @@
+require(data.table, quietly = T)
+require(ggplot2, quietly = T)
+
+scc <- readRDS("summarySCC_PM25.rds")
+codes <- readRDS("Source_Classification_Code.rds")
+
+codes.vehicle <- codes[grepl("Vehicle", codes$SCC.Level.Two), 1]
+
+scc.CA <- subset(scc, SCC %in% codes.vehicle & fips == "24510")
+scc.MD <- subset(scc, SCC %in% codes.vehicle & fips == "06037")
+
+agg.scc.CA <- aggregate(Emissions ~ year + fips, scc.CA, sum)
+agg.scc.MD <- aggregate(Emissions ~ year + fips, scc.MD, sum)
+
+agg.data <- rbind(agg.scc.CA, agg.scc.MD)
+
+agg.data$fips <- as.factor(agg.data$fips)
+levels(agg.data$fips) <- c("Los Angeles", "Baltimore")
+
+png(filename = "plot6.png", height = 600, width = 600)
+
+p <- qplot(year, Emissions, data = agg.data) 
+p <- p + aes(color = fips) 
+p <- p + geom_smooth()
+print(p)
+
+dev.off()
