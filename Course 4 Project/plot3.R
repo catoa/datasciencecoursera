9c9abbd2334cee6466f69fa7f571dcdb582b1584
Anthony Cato <catoa0608@gmail.com>
2017-03-08 13:38:42 -0500

    Make small changes to plot3 and plot5 code

--- Course 4 Project/plot3.R
+++ Course 4 Project/plot3.R
@@ -6,11 +6,11 @@ scc <- subset(scc, fips == "24510")
 
 agg <- aggregate(Emissions ~ year + type, scc, sum)
 
-png(filename = "plot3.png", height = 500, width = 800)
+# png(filename = "plot3.png", height = 500, width = 800)
 
 p <- qplot(year, Emissions, data = agg) 
 p <- p + aes(color = type) 
 p <- p + geom_smooth()
 print(p)
-dev.off()
+# dev.off()
 
--- Course 4 Project/plot5.R
+++ Course 4 Project/plot5.R
@@ -12,7 +12,7 @@ p <- ggplot(data = agg, aes(factor(year), Emissions))
 p <- p + geom_bar(stat="identity") +
     xlab("year") +
     ylab(expression('Total PM'[2.5]*" Emissions")) +
-    ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+    ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland')
 print(p)
 dev.off()
 
